d710f9ea15f0bf980c1fa8772572744a0328dab0
Ali Mansour <alialhadimansour21@gmail.com>
2024-04-27 01:24:43 +0300

    Implement the sign up screen

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -15,9 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          // useMaterial3: true,
+          cardTheme: const CardTheme(color: Colors.white)),
       routes: {
         '/': (context) => const LoginScreen(),
         '/register': (context) => const RegisterScreen()
--- mobile/lib/screens/register_screen.dart
+++ mobile/lib/screens/register_screen.dart
@@ -6,7 +6,78 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  children: <Widget>[
+                    const Spacer(),
+                    Card(
+                      color: Colors.white,
+                      margin: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const FlutterLogo(size: 80),
+                          const SizedBox(height: 20),
+                          const Text(
+                            "Let’s Get Started",
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          const SizedBox(height: 20),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Name',
+                              prefixIcon: Icon(Icons.person),
+                            ),
+                            keyboardType: TextInputType.name,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: "Email",
+                              prefixIcon: Icon(Icons.email),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                              prefixIcon: Icon(Icons.lock),
+                            ),
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                              onPressed: () {
+                                // Sign up button logic
+                              },
+                              child: const Text("Sign up")),
+                          const SizedBox(height: 20),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacementNamed(context, '/');
+                            },
+                            child: const Text(
+                              "Have an account? LogIn",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(), // Use another Spacer to push the Card to the middle
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
