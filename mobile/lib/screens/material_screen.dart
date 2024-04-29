b745c0ef88b2e65a654bd47bc69b9b1370babda2
Ali Mansour <alialhadimansour21@gmail.com>
2024-04-29 17:05:58 +0300

    Create the material screen and display the data

--- mobile/lib/screens/material_screen.dart
+++ mobile/lib/screens/material_screen.dart
@@ -16,8 +16,35 @@ class MaterialScreen extends StatelessWidget {
         ),
         title: Text(
           material.title,
-          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
         ),
+      ),
+      body: ListView.separated(
+        itemCount: material.topics.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              material.topics[index],
+              style: const TextStyle(color: Colors.black),
+            ),
+            trailing: const Icon(
+              Icons.chevron_right,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              // Handle the tap here
+            },
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(
+            color: Colors.grey.shade300,
+            height: 1,
+            thickness: 1,
+            indent: 16,
+            endIndent: 16,
+          );
+        },
       ),
     );
   }
