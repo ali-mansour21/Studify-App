a33ee1401ed89cb5bbe387da3f092601915c4033
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-01 18:02:12 +0300

    Implement the image picker

--- mobile/lib/screens/edit_screen.dart
+++ mobile/lib/screens/edit_screen.dart
@@ -1,11 +1,30 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mobile/widgets/customtextformfield.dart';
 import 'package:mobile/widgets/mainbutton.dart';
 import 'package:mobile/widgets/profile_screen_layout.dart';
 
-class EditScreen extends StatelessWidget {
+class EditScreen extends StatefulWidget {
   const EditScreen({super.key});
+
+  @override
+  _EditScreenState createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  File? _imageFile;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,67 +33,54 @@ class EditScreen extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            size: 24,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+          icon: const Icon(Icons.arrow_back, size: 24, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
         ),
         elevation: 0,
         backgroundColor: Colors.transparent,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF3786A8), Color(0xFF3786A8)],
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-            ),
-          ),
-        ),
-        title: const Text(
-          "Edit Profile",
-          style: TextStyle(fontSize: 18, color: Colors.white),
-        ),
+        title: const Text("Edit Profile",
+            style: TextStyle(fontSize: 18, color: Colors.white)),
       ),
       body: ProfileLayout(
         height: 600,
         children: [
-          Stack(
-            clipBehavior: Clip.none,
-            alignment: Alignment.center,
-            children: [
-              const Positioned(
-                top: -65,
-                left: 0,
-                right: 0,
-                child: CircleAvatar(
-                  radius: 60,
-                  child: Text('AM',
-                      style: TextStyle(fontSize: 24, color: Colors.white)),
-                ),
-              ),
-              Positioned(
-                top: -65,
-                left: 0,
-                right: 0,
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                  child: const Icon(
-                    Icons.edit,
-                    color: Colors.white,
-                    size: 30,
+          GestureDetector(
+            onTap: _pickImage,
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  top: -65,
+                  left: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage:
+                        _imageFile == null ? null : FileImage(_imageFile!),
+                    child: _imageFile == null
+                        ? const Text('AM',
+                            style: TextStyle(fontSize: 24, color: Colors.white))
+                        : null,
                   ),
                 ),
-              ),
-            ],
+                Positioned(
+                  top: -65,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                    child:
+                        const Icon(Icons.edit, color: Colors.white, size: 40),
+                  ),
+                ),
+              ],
+            ),
           ),
           Center(
             child: Column(
