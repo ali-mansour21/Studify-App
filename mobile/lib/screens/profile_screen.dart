0e1aa8d177d0fc43d51b6d920168202986b69c9b
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-01 14:09:20 +0300

    Use the layout in the profile  screen

--- mobile/lib/screens/profile_screen.dart
+++ mobile/lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/widgets/navigation_bar.dart';
+import 'package:mobile/widgets/profile_screen_layout.dart';
 import 'package:mobile/widgets/state_layout.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -56,103 +57,88 @@ class _ProfileScreenState extends State<ProfileScreen> {
           )
         ],
       ),
-      body: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: 500, 
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(35),
-              topRight: Radius.circular(35),
+      body: ProfileLayout(
+        children: [
+          const Positioned(
+            top: -85,
+            left: 0,
+            right: 0,
+            child: CircleAvatar(
+              radius: 60,
+              child: Text('AM',
+                  style: TextStyle(fontSize: 24, color: Colors.white)),
             ),
           ),
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              const Positioned(
-                top: -85,
-                left: 0,
-                right: 0,
-                child: CircleAvatar(
-                  radius: 60,
-                  child: Text('AM',
-                      style: TextStyle(fontSize: 24, color: Colors.white)),
-                ),
+          const Positioned(
+            top: 45,
+            left: 0,
+            right: 0,
+            child: Text(
+              'Ali Mansour',
+              style: TextStyle(
+                fontSize: 21,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
               ),
-              const Positioned(
-                top: 45,
-                left: 0,
-                right: 0,
-                child: Text(
-                  'Ali Mansour',
-                  style: TextStyle(
-                    fontSize: 21,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const Positioned(
-                top: 140,
-                left: 0,
-                right: 0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    StateWidget(
-                      count: '13',
-                      label: 'Rank',
-                      icon: Icons.star,
-                    ),
-                    StateWidget(
-                      count: '13',
-                      label: 'Documents',
-                      icon: Icons.document_scanner,
-                    ),
-                    StateWidget(
-                      count: '13',
-                      label: 'Comments',
-                      icon: Icons.comment,
-                    ),
-                  ],
-                ),
-              ),
-              ListView(
-                padding: const EdgeInsets.only(top: 200),
-                shrinkWrap: true,
-                children: ListTile.divideTiles(context: context, tiles: [
-                  ListTile(
-                    leading: const Icon(Icons.edit),
-                    title: const Text('Edit Profile'),
-                    trailing: const Icon(Icons.arrow_forward_ios),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.book),
-                    title: const Text('Material'),
-                    trailing: const Icon(Icons.arrow_forward_ios),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.class_),
-                    title: const Text('Classes'),
-                    trailing: const Icon(Icons.arrow_forward_ios),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.logout),
-                    title: const Text('Logout'),
-                    trailing: const Icon(Icons.arrow_forward_ios),
-                    onTap: () {},
-                  ),
-                ]).toList(),
-              )
-            ],
+              textAlign: TextAlign.center,
+            ),
           ),
-        ),
+          const Positioned(
+            top: 140,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                StateWidget(
+                  count: '13',
+                  label: 'Rank',
+                  icon: Icons.star,
+                ),
+                StateWidget(
+                  count: '13',
+                  label: 'Documents',
+                  icon: Icons.document_scanner,
+                ),
+                StateWidget(
+                  count: '13',
+                  label: 'Comments',
+                  icon: Icons.comment,
+                ),
+              ],
+            ),
+          ),
+          ListView(
+            padding: const EdgeInsets.only(top: 200),
+            shrinkWrap: true,
+            children: ListTile.divideTiles(context: context, tiles: [
+              ListTile(
+                leading: const Icon(Icons.edit),
+                title: const Text('Edit Profile'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.book),
+                title: const Text('Material'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.class_),
+                title: const Text('Classes'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Logout'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+                onTap: () {},
+              ),
+            ]).toList(),
+          )
+        ],
       ),
       bottomNavigationBar: CustomNavigationBar(
         currentIndex: _selectedIndex,
