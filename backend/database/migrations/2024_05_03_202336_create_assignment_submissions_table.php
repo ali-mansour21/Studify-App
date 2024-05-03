74e4a9da3d00724576f87241018a0168d56c0dd0
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-03 23:28:50 +0300

    Create the  submissions & submission feedback  model & migration

---
+++ backend/database/migrations/2024_05_03_202336_create_assignment_submissions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignment_submissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('solution');
+            $table->foreignId('student_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('assignment_id')->references('id')->on('assignments')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignment_submissions');
+    }
+};
---
+++ backend/database/migrations/2024_05_03_202610_create_submission_feedback_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('submission_feedback', function (Blueprint $table) {
+            $table->id();
+            $table->text('feedback');
+            $table->foreignId('assignment_id')->references('id')->on('assignments')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submission_feedback');
+    }
+};
