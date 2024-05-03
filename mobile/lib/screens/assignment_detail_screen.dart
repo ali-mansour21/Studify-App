95290d34cb92e010bfe7bf8870b444a223059f97
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-03 21:11:17 +0300

    Display the assignment data

--- mobile/lib/screens/assignment_detail_screen.dart
+++ mobile/lib/screens/assignment_detail_screen.dart
@@ -12,16 +12,41 @@ class AssignmentDetailScreen extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.white,
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.arrow_back,
-              size: 24,
-            )),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 24,
+          ),
+        ),
         title: Text(
           assignment.title,
           style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24),
+            topRight: Radius.circular(24),
+          ),
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 8),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  assignment.content,
+                  style: const TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
