ff2e50e1207b8244337f393a34f7acea0acd6a79
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-03 13:52:28 +0300

    Add the assignment  section

--- mobile/lib/screens/class_material_screen.dart
+++ mobile/lib/screens/class_material_screen.dart
@@ -18,7 +18,7 @@ class _ClassMaterialDetailScreenState extends State<ClassMaterialDetailScreen> {
   Widget build(BuildContext context) {
     List<Widget> content = [
       _buildTopicList(widget.material.topics),
-      // _buildAssignmentList(widget.material.assignments),
+      _buildAssignmentList(widget.material.assignments),
     ];
     return Scaffold(
       appBar: AppBar(
@@ -46,6 +46,7 @@ class _ClassMaterialDetailScreenState extends State<ClassMaterialDetailScreen> {
                 groupValue: _selectedIndex,
               ),
             ),
+            const SizedBox(height: 20),
             Expanded(child: content[_selectedIndex]),
           ],
         ),
@@ -62,6 +63,39 @@ Widget _buildTopicList(List<model.Topic> topics) {
       return ListTile(
         title: Text(
           topic.title,
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        trailing: const Icon(
+          Icons.chevron_right,
+          color: Colors.grey,
+        ),
+        onTap: () {},
+      );
+    },
+    separatorBuilder: (context, index) {
+      return Divider(
+        color: Colors.grey.shade300,
+        height: 1,
+        thickness: 1,
+        indent: 16,
+        endIndent: 16,
+      );
+    },
+  );
+}
+
+Widget _buildAssignmentList(List<model.Assignment> assignments) {
+  return ListView.separated(
+    itemCount: assignments.length,
+    itemBuilder: (context, index) {
+      final assignment = assignments[index];
+      return ListTile(
+        title: Text(
+          assignment.title,
           style: const TextStyle(
             color: Colors.black,
             fontWeight: FontWeight.bold,
