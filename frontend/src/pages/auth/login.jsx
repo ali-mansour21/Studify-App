7ce9965d33ef56ac3b25009cb0201f2f0645c1d7
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-04 22:01:44 +0300

    Style the login page

--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import "../../styles/utilities.css";
+import { useNavigate } from "react-router-dom";
+import "./styles/login.css";
 const Login = () => {
+  const navigator = useNavigate();
   return (
     <div>
       <div className="signin-container">
-        <h2>Sign In</h2>
         <div className="signin-logo">
-          <img src="" alt="Logo" />
+          <img src="../src/assets/Studify-logo.png" alt="Logo" />
         </div>
+        <h2>Sign In to your account</h2>
         <form className="signin-form">
           <input type="email" placeholder="Enter your email" required />
           <input type="password" placeholder="Password" required />
           <button type="submit">Sign In</button>
         </form>
         <div className="signup-link">
-          New user? <a href="/signup">Sign Up</a>
+          New user?{" "}
+          <a
+            onClick={() => {
+              navigator("/register");
+            }}
+          >
+            Sign Up
+          </a>
         </div>
       </div>
     </div>
