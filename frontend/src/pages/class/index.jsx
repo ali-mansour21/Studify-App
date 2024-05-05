f81d9ed91bafe5c813c52310f8825355e30f4fe7
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-05 16:48:32 +0300

    Create the classes page

---
+++ frontend/src/pages/class/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SideBar from "../home/components/sidebar";
+import "../../styles/utilities.css";
+
+const Class = () => {
+  return (
+    <div className="page d-flex">
+      <SideBar />
+      <div className="content w-full">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Class;
