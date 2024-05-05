10eb3eb62e3c0f5add9d627127621f81bea3a5ac
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-05 17:24:28 +0300

    Create the add class button

--- frontend/src/pages/class/index.jsx
+++ frontend/src/pages/class/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import SideBar from "../../components/sidebar";
 import "../../styles/utilities.css";
+import "../../styles/index.css";
 import Header from "../../components/header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
@@ -9,6 +12,16 @@ const Home = () => {
       <SideBar />
       <div className="content w-full">
         <Header />
+        <div className="d-flex spacebetween">
+          <h1 className="p-relative">Classes</h1>
+          <div className="actions d-flex gap-10">
+            <button>
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
+            <p>Add</p>
+          </div>
+        </div>
+        <div className="classes-page d-grid gap-20 m-20"></div>
       </div>
     </div>
   );
