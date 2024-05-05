52fd3a574f488aac89b508818c9c9da2438f1637
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-05 21:36:27 +0300

    Implement the suggested notes function

---
+++ backend/app/Http/Controllers/student/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\student;
+
+use App\Http\Controllers\Controller;
+use App\Models\StudentNote;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $user = auth()->user();
+        $categories = $user->categories;
+        $student_notes = StudentNote::whereHas('categories', function ($query) use ($categories) {
+            $query->whereIn('id', $categories->pluck('id'));
+        })->get();
+    }
+}
