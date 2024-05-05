2f40771658bd6b961b63e9432d113bfcec444520
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-05 16:44:54 +0300

    Add data to the main page

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -19,6 +19,25 @@ const Home = () => {
             <img src="profile_image" alt="" />
           </div>
         </div>
+        <h1 className="p-relative">Dashboard</h1>
+        <div className="dasboard-page d-grid gap-20 m-20">
+          <div className="box  bg-white rad-6 p-relative">
+            <h2>Number of classes</h2>
+            <p>20</p>
+          </div>
+          <div className="box  bg-white rad-6 p-relative">
+            <h2>Number of classes</h2>
+            <p>20</p>
+          </div>
+          <div className="box  bg-white rad-6 p-relative">
+            <h2>Number of classes</h2>
+            <p>20</p>
+          </div>
+          <div className="box  bg-white rad-6 p-relative">
+            <h2>Number of classes</h2>
+            <p>20</p>
+          </div>
+        </div>
       </div>
     </div>
   );
