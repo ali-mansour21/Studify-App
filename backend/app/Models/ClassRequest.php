33e4cfaf9280bade236f81d80121d577bd4d6dd4
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-06 17:09:11 +0300

    Create the class_request table

---
+++ backend/app/Models/ClassRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassRequest extends Model
+{
+    use HasFactory;
+    protected $fillable = ['student_id', 'class_id', 'status'];
+}
