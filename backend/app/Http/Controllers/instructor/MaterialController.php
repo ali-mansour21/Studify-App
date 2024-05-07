798553cc9b0b755e50b9602b06927f4adb6a2c2a
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-07 22:38:31 +0300

    Implement the create assignment function

--- backend/app/Http/Controllers/instructor/MaterialController.php
+++ backend/app/Http/Controllers/instructor/MaterialController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\instructor;
 
 use App\Http\Controllers\Controller;
+use App\Models\Assignment;
 use App\Models\Material;
 use App\Models\Topic;
 use Illuminate\Http\Request;
@@ -31,13 +32,22 @@ class MaterialController extends Controller
             'content' => ['required', 'string', 'min:10'],
             'type' => ['required', 'integer']
         ]);
-        if($data['type'] == 0){
+        if ($data['type'] == 0) {
             $topic = new Topic();
             $topic->title = $data['title'];
             $topic->content = $data['content'];
             $topic->material_id = $data['material_id'];
             $topic->save();
             return response()->json(['message' => 'Topic created successfully'], 201);
+        } elseif ($data['type'] == 1) {
+            $assignment = new Assignment();
+            $assignment->title = $data['title'];
+            $assignment->content = $data['content'];
+            $assignment->material_id = $data['material_id'];
+            $assignment->save();
+            return response()->json(['message' => 'Assignment created successfully'], 201);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Invalid type provided'], 400);
         }
     }
 }
