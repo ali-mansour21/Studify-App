a77d437052575dff5de2da51dbf1271692caca71
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-08 12:20:46 +0300

    Handle the login

--- backend/app/Http/Controllers/instructor/AuthController.php
+++ backend/app/Http/Controllers/instructor/AuthController.php
@@ -48,4 +48,33 @@ class AuthController extends Controller
             ]
         ]);
     }
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email', Rule::exists('users', 'email')],
+            'password' => [
+                'required',
+                'min:6',
+                'max:16',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/'
+            ],
+        ]);
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
 }
