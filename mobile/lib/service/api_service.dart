c1c62d338557922cbf5a07b578718f974a2c353a
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-08 16:33:17 +0300

    Create the login function

---
+++ mobile/lib/service/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = "http://192.168.0.104:8001/api";
+  Future<dynamic> login(String email, String password) async {
+    try {
+      final response = await http.post(Uri.parse('$baseUrl/student_login'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: json.encode({
+            'email': email,
+            'password': password,
+          }));
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Failed to login: $response.body');
+      }
+    } catch (e) {
+      throw Exception('Failed to login: $e');
+    }
+  }
+}
