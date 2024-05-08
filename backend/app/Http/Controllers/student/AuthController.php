67f7db2d00e9f900af2612c99dd04649fa2b3cf5
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-09 02:18:27 +0300

    Create a auth register request

--- backend/app/Http/Controllers/student/AuthController.php
+++ backend/app/Http/Controllers/student/AuthController.php
@@ -13,15 +13,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $data = $request->validate([
-            'name' => ['required', 'string', 'min:2', 'max:50'],
-            'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => [
-                'required',
-                'min:6',
-                'max:16',
-                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/'
-            ],
-            'firebase_token' => ['required', 'string']
+            
         ]);
         $user = new User();
         $user->name = $data['name'];
---
+++ backend/app/Http/Requests/student/AuthRegisterRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\student;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class AuthRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:2', 'max:50'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => [
+                'required',
+                'min:6',
+                'max:16',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/'
+            ],
+            'firebase_token' => ['required', 'string']
+        ];
+    }
+}
