da55a7784e7f30a1dae6bc1ab137f1af86d97871
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-08 12:20:01 +0300

    Handle the create instructor

--- backend/app/Http/Controllers/instructor/AuthController.php
+++ backend/app/Http/Controllers/instructor/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\student;
+namespace App\Http\Controllers\instructor;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -21,14 +21,21 @@ class AuthController extends Controller
                 'max:16',
                 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/'
             ],
-            'firebase_token' => ['required', 'string']
+            'firebase_token' => ['required', 'string'],
+            'profile_image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048']
         ]);
         $user = new User();
         $user->name = $data['name'];
         $user->email = $data['email'];
         $user->password = bcrypt($data['password']);
-        $user->user_type = 'student';
+        $user->user_type = 'instructor';
         $user->firebase_token = $data['firebase_token'];
+        if ($request->hasFile('profile_image')) {
+            $file = $request->file('profile_image');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs('profile_images', $filename, 'public');
+            $user->profile_image = $path;
+        }
         $user->save();
         $token = Auth::login($user);
         return response()->json([
@@ -39,43 +46,6 @@ class AuthController extends Controller
                 'token' => $token,
                 'type' => 'bearer',
             ]
-        ]);
-    }
-    public function login(Request $request)
-    {
-        $credentials = $request->validate([
-            'email' => ['required', 'email', Rule::exists('users', 'email')],
-            'password' => [
-                'required',
-                'min:6',
-                'max:16',
-                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/'
-            ],
-        ]);
-        $token = Auth::attempt($credentials);
-        if (!$token) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized',
-            ], 401);
-        }
-
-        $user = Auth::user();
-        return response()->json([
-            'status' => 'success',
-            'user' => $user,
-            'authorisation' => [
-                'token' => $token,
-                'type' => 'bearer',
-            ]
-        ]);
-    }
-    public function logout()
-    {
-        Auth::logout();
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Successfully logged out',
         ]);
     }
 }
