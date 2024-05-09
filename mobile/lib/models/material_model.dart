ed2e0525917563392c950bb24dbde8207e371aad
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-09 15:45:09 +0300

    Create a material provider

--- mobile/lib/models/material_model.dart
+++ mobile/lib/models/material_model.dart
@@ -1,9 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:mobile/models/topic_material.dart';
 
-class MaterialItem {
-  final int id;
-  final String title;
-  final List<NotesTopic> topics;
+class MaterialItemProvider with ChangeNotifier {
+  int _id;
+  String _title;
+  List<NotesTopic> _topics;
 
-  MaterialItem({required this.id, required this.title, required this.topics});
+  MaterialItemProvider(
+      {required int id,
+      required String title,
+      required List<NotesTopic> topics})
+      : _id = id,
+        _title = title,
+        _topics = topics;
+
+  int get id => _id;
+  String get title => _title;
+  List<NotesTopic> get topics => _topics;
+
+  void updateTitle(String newTitle) {
+    _title = newTitle;
+    notifyListeners();
+  }
+
+  void updateTopics(List<NotesTopic> newTopics) {
+    _topics = newTopics;
+    notifyListeners();
+  }
 }
--- mobile/lib/models/topic_material.dart
+++ mobile/lib/models/topic_material.dart
@@ -1,14 +1,34 @@
+import 'package:flutter/material.dart';
+
 abstract class Topic {
   String get title;
   String get content;
 }
 
-class NotesTopic implements Topic {
-  final int id;
-  @override
-  final String title;
-  @override
-  final String content;
+class NotesTopicProvider with ChangeNotifier implements Topic {
+  int _id;
+  String _title;
+  String _content;
 
-  NotesTopic({required this.id, required this.title, required this.content});
+  NotesTopicProvider(
+      {required int id, required String title, required String content})
+      : _id = id,
+        _title = title,
+        _content = content;
+
+  int get id => _id;
+  String get title => _title;
+  String get content => _content;
+
+  // Method to update the title and notify listeners
+  void updateTitle(String newTitle) {
+    _title = newTitle;
+    notifyListeners();
+  }
+
+  // Method to update the content and notify listeners
+  void updateContent(String newContent) {
+    _content = newContent;
+    notifyListeners();
+  }
 }
