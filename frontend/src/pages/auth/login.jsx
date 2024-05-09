236e7345f7b9fe537a1061ad41e3a92804a233af
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-09 23:16:55 +0300

    Create the login function

--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
+import sendRequest from "../../core/tools/userRequest.js";
+import { requestMethods } from "../../core/requests/requestMethods.js";
 import "./styles/login.css";
 const Login = () => {
   const navigator = useNavigate();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleLogin = () => {
+    sendRequest(requestMethods.POST, "instructor_login", userData).then(
+      (response) => {
+        if (response.status === 200) {
+          localStorage.setItem("token", response.data.authorization.token);
+          navigator("/home");
+        }
+      }
+    );
+  };
   return (
     <div>
       <div className="signin-container">
@@ -12,16 +28,36 @@ const Login = () => {
           <img src="../src/assets/Studify-logo.png" alt="Logo" />
         </div>
         <h2>Sign In to your account</h2>
-        <form className="signin-form">
+        <form
+          className="signin-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
+        >
           <div>
-            <input type="email" placeholder="john@gmail.com" required />
+            <input
+              type="email"
+              onChange={(e) => {
+                setUserData({ ...userData, email: e.target.value });
+              }}
+              placeholder="john@gmail.com"
+              required
+            />
             <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
           </div>
           <div>
-            <input type="password" placeholder="password" required />
+            <input
+              type="password"
+              onChange={(e) => {
+                setUserData({ ...userData, password: e.target.value });
+              }}
+              placeholder="password"
+              required
+            />
             <FontAwesomeIcon icon={faKey} className="input-icon" />
           </div>
-          <button type="submit">Sign In</button>
+          <button type="submit">Login</button>
         </form>
         <div className="signup-link">
           New user?{" "}
