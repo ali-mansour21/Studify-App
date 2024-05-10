652c4cbca08d9b2cb311d136b94d8072fc5887c0
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-10 11:15:16 +0300

    Implement the create instructor function

--- frontend/src/pages/auth/signup.jsx
+++ frontend/src/pages/auth/signup.jsx
@@ -5,12 +5,42 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import sendRequest from "../../core/tools/userRequest.js";
+
 import "./styles/signup.css";
+import { requestMethods } from "../../core/requests/requestMethods.js";
 const SignUp = () => {
   const navigator = useNavigate();
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profile_image: null,
+  });
+  const uploadImage = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertBase64(file);
+    setUserData({ ...userData, profile_image: base64 });
+  };
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
 
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+  const handleSubmit = () => {
+    sendRequest(requestMethods.POST, "instructor_register", userData);
+  };
   return (
     <div>
       <div className="signup-container">
@@ -18,21 +48,48 @@ const SignUp = () => {
           <img src="../src/assets/Studify-logo.png" alt="Logo" />
         </div>
         <h2>Create new account</h2>
-        <form className="signup-form">
+        <form
+          className="signup-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
           <div>
-            <input type="text" placeholder="John Doe" required />
+            <input
+              type="text"
+              onChange={(e) => {
+                setUserData({ ...userData, name: e.target.value });
+              }}
+              placeholder="John Doe"
+              required
+            />
             <FontAwesomeIcon icon={faUser} className="input-icon" />
           </div>
           <div>
-            <input type="email" placeholder="john@gmail.com" required />
+            <input
+              type="email"
+              onChange={(e) => {
+                setUserData({ ...userData, email: e.target.value });
+              }}
+              placeholder="john@gmail.com"
+              required
+            />
             <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
           </div>
           <div>
-            <input type="file" required />
+            <input type="file" onChange={(e) => uploadImage(e)} required />
             <FontAwesomeIcon icon={faImage} className="input-icon" />
           </div>
           <div>
-            <input type="password" placeholder="password" required />
+            <input
+              type="password"
+              onChange={(e) => {
+                setUserData({ ...userData, password: e.target.value });
+              }}
+              placeholder="Password"
+              required
+            />
             <FontAwesomeIcon icon={faKey} className="input-icon" />
           </div>
           <button type="submit">Sign Up</button>
