992319651f23ba31a4d094a8fc0874789258442f
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-11 16:23:12 +0300

    Create a function to fetch student created notes

--- mobile/lib/providers/material_provider.dart
+++ mobile/lib/providers/material_provider.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/models/material_model.dart';
 import 'package:mobile/services/home_api_service.dart';
+import 'package:mobile/services/profile_api.dart';
 
 class MaterialsProvider with ChangeNotifier {
   final HomeApiService _apiService = HomeApiService();
+  final ProfileApiService _studentApiService = ProfileApiService();
 
   List<MaterialItem> _materials = [];
+  List<MaterialItem> _studentMaterials = [];
   bool _isLoading = false;
 
   List<MaterialItem> get materials => _materials;
@@ -16,6 +19,19 @@ class MaterialsProvider with ChangeNotifier {
     notifyListeners();
     try {
       _materials = await _apiService.getNotesData(context);
+    } catch (e) {
+      print("Failed to fetch materials: $e");
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void fetchStudnetMaterials(BuildContext context) async {
+    _isLoading = true;
+    notifyListeners();
+    try {
+      _studentMaterials = await _studentApiService.getStudentNotesData(context);
     } catch (e) {
       print("Failed to fetch materials: $e");
     } finally {
---
+++ mobile/lib/services/profile_api.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile/models/users/user_data.dart';
+import 'package:mobile/models/material_model.dart';
+import 'package:provider/provider.dart';
+
+class ProfileApiService {
+  final String baseUrl = "http://192.168.0.104:8001/api";
+
+  Future<List<MaterialItem>> getStudentNotesData(BuildContext context) async {
+    String token = Provider.of<UserData>(context, listen: false).jwtToken;
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/studentNotes'),
+        headers: {'Authorization': 'Bearer $token'},
+      );
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        if (data['status'] == 'success' && data['data'] != null) {
+          List<dynamic> notesJson = data['data']['recommended_notes'];
+          List<MaterialItem> studentNotes = notesJson
+              .map((dynamic item) => MaterialItem.fromJson(item))
+              .cast<MaterialItem>()
+              .toList();
+          return studentNotes;
+        } else {
+          throw Exception("No notes data found or failed status");
+        }
+      } else {
+        throw Exception(
+            'Failed to load materials. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to get home data: $e');
+    }
+  }
+}
