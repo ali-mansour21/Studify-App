893f07a4d181310e5353161f42de06a29a9c900a
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-11 17:04:23 +0300

    Add a condition if there is no classes

--- mobile/lib/screens/student_class_screen.dart
+++ mobile/lib/screens/student_class_screen.dart
@@ -23,41 +23,51 @@ class StudentClassScreen extends StatelessWidget {
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
           ),
         ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: ListView.builder(
-                itemCount: studentClasses.length,
-                itemBuilder: (context, index) {
-                  final classData = studentClasses[index];
-                  return Card(
-                    child: ListTile(
-                      leading: const Icon(
-                        Icons.class_,
-                        color: Colors.blue,
-                        size: 50,
-                      ),
-                      title: Text(classData.title),
-                      subtitle: Text(classData.description),
-                      trailing: const Icon(Icons.chevron_right),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ClassDetailScreen(
-                                    classDetail: classData,
-                                    isInClass: true,
-                                  )),
+        body: studentClasses.isEmpty
+            ? const Center(
+                child: Text(
+                  'No materials available.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+              )
+            : Column(
+                children: <Widget>[
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: ListView.builder(
+                      itemCount: studentClasses.length,
+                      itemBuilder: (context, index) {
+                        final classData = studentClasses[index];
+                        return Card(
+                          child: ListTile(
+                            leading: const Icon(
+                              Icons.class_,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
+                            title: Text(classData.title),
+                            subtitle: Text(classData.description),
+                            trailing: const Icon(Icons.chevron_right),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ClassDetailScreen(
+                                          classDetail: classData,
+                                          isInClass: true,
+                                        )),
+                              );
+                            },
+                          ),
                         );
                       },
                     ),
-                  );
-                },
-              ),
-            )),
-          ],
-        ));
+                  )),
+                ],
+              ));
   }
 }
