74922f15a97d42f7df07cadbdeb45215e04799af
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-11 14:51:29 +0300

    Fix the frontend fcm configuration

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAL6EzcAuxkNBQibIAG5hJJBBRzKXRMx-Q",
-  authDomain: "studify-81155.firebaseapp.com",
-  projectId: "studify-81155",
-  storageBucket: "studify-81155.appspot.com",
-  messagingSenderId: "607925811224",
-  appId: "1:607925811224:web:506b746d56d2a642cf6b6d",
-  measurementId: "G-5BGNMM18DT",
+  apiKey: "AIzaSyAUmAIvc568vkPamF6XtU3LqaMLkpwEdJc",
+  authDomain: "studify-notifications.firebaseapp.com",
+  databaseURL: "https://studify-notifications-default-rtdb.firebaseio.com",
+  projectId: "studify-notifications",
+  storageBucket: "studify-notifications.appspot.com",
+  messagingSenderId: "256089037545",
+  appId: "1:256089037545:web:008abf3f8fefe7a505ae78",
 };
 
 // Initialize Firebase
@@ -19,7 +19,7 @@ export const generateToken = async () => {
   await Notification.requestPermission();
   const token = await getToken(messaging, {
     vapidKey:
-      "BAmmOeneLRRARtqO7a3AX2M2HYrlIJT3b1iktYq7EfZAO0MGW0zRLpI90mbiqQgrK31aooCvHqWNIzO22wllM1Y",
+      "BA3Xnsyb1PZEb1BBve8ZkWDQ3xGmdtbH4B4Zi6XKpfx-_g-ccrci-tIeMCgKwQCCfiE3Tw5DrvzY-095V4SQDoM",
   });
   return token;
 };
