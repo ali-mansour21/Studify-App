d265f43dd086b51c22b32ae844d9d988678d2e70
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-11 17:27:19 +0300

    Create the notification provider

--- mobile/lib/providers/notification_provider.dart
+++ mobile/lib/providers/notification_provider.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/widgets.dart';
+import 'package:mobile/models/notifications/notification_model.dart';
+import 'package:mobile/services/home_api_service.dart';
 
-class NotificationProvider with ChangeNotifier{
-  
+class NotificationProvider with ChangeNotifier {
+  final HomeApiService _apiService = HomeApiService();
+  List<UserNotification> _notifications = [];
+  bool _isLoading = false;
+  List<UserNotification> get notifications => _notifications;
+  bool get isLoading => _isLoading;
+  Future<void> getNotifications(BuildContext context) async {
+    try {
+      _notifications = await _apiService.getNotifications(context);
+      notifyListeners();
+    } catch (e) {
+      print('Failed to load classes: $e');
+    }
+  }
 }
