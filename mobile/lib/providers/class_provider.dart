59cd59a674f2c42220d6634278147d0c3b715abe
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-11 16:43:29 +0300

    Create a function to fetch student classes

--- mobile/lib/providers/class_provider.dart
+++ mobile/lib/providers/class_provider.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/models/classes/class_data.dart';
 import 'package:mobile/services/home_api_service.dart';
+import 'package:mobile/services/profile_api.dart';
 
 class StudyClassProvider with ChangeNotifier {
   final HomeApiService _apiService = HomeApiService();
+  final ProfileApiService _profileApiService = ProfileApiService();
   List<ClassData> _studyClasses = [];
+  List<ClassData> _studentStudyClasses = [];
   bool _isLoading = false;
 
   List<ClassData> get studyClasses => _studyClasses;
+  List<ClassData> get studentStudyClass => _studentStudyClasses;
   bool get is_loading => _isLoading;
   Future<void> loadClasses(BuildContext context) async {
     try {
       _studyClasses = await _apiService.getClassesData(context);
+      notifyListeners();
+    } catch (e) {
+      print('Failed to load classes: $e');
+    }
+  }
+  Future<void> loadStudentClasses(BuildContext context) async {
+    try {
+      _studentStudyClasses = await _apiService.getClassesData(context);
       notifyListeners();
     } catch (e) {
       print('Failed to load classes: $e');
--- mobile/lib/services/profile_api.dart
+++ mobile/lib/services/profile_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile/models/classes/class_data.dart';
 import 'package:mobile/models/users/user_data.dart';
 import 'package:mobile/models/material_model.dart';
 import 'package:provider/provider.dart';
@@ -30,6 +31,32 @@ class ProfileApiService {
       } else {
         throw Exception(
             'Failed to load materials. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to get home data: $e');
+    }
+  }
+
+  Future<List<ClassData>> getClassesData(BuildContext context) async {
+    String token = Provider.of<UserData>(context, listen: false).jwtToken;
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/resources'),
+        headers: {'Authorization': 'Bearer $token'},
+      );
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        if (data['status'] == 'success' && data['data'] != null) {
+          List<dynamic> jsonData = data['data'];
+          return jsonData
+              .map((classJson) => ClassData.fromJson(classJson))
+              .toList();
+        } else {
+          throw Exception('No classes data found or failed status');
+        }
+      } else {
+        throw Exception(
+            'Failed to load classes. Status code: ${response.statusCode}');
       }
     } catch (e) {
       throw Exception('Failed to get home data: $e');
