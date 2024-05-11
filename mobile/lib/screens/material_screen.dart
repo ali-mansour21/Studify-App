e927ca7bec8eb75da96f36f76f2ed163b3bc4326
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-11 15:52:10 +0300

    Install the download files packages

--- mobile/lib/screens/material_screen.dart
+++ mobile/lib/screens/material_screen.dart
@@ -8,6 +8,7 @@ class MaterialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
