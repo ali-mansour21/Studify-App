c94ee7e67263e43a2a0ef5d48d8dd59ecc554c6a
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-13 13:48:58 +0300

    Create a function to fetch classes

--- frontend/src/redux/boarderSlice.js
+++ frontend/src/redux/boarderSlice.js
@@ -1,5 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import sendAuthRequest from "../core/tools/authRequest";
+import { requestMethods } from "../core/requests/requestMethods";
+export const fetchClasses = createAsyncThunk(
+  "user/fetchClasses",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await sendAuthRequest(requestMethods.GET, "classes");
+      if (!response.ok) throw new Error("Network response was not ok");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
 const boardsSlice = createSlice({
   name: "user",
   initialState: {},
