3cbcd6a5c1ad0cc379d80d728d1e802ef9087d0f
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-14 12:25:39 +0300

    Create the materials page

---
+++ frontend/src/pages/class/components/Materials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../styles/utilities.css";
+import "../../styles/index.css";
+import SideBar from '../../../components/sidebar';
+const Materials = () => {
+  return (
+    <div className="page d-flex">
+      <SideBar />
+    </div>
+  );
+}
+
+export default Materials
