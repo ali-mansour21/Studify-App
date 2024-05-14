aee7774b59079a13fe8fd63d61ec08b549722c6f
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-14 12:29:52 +0300

    Add the sidebar and the header

--- frontend/src/pages/class/components/Materials.jsx
+++ frontend/src/pages/class/components/Materials.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import "../../styles/utilities.css";
 import "../../styles/index.css";
 import SideBar from '../../../components/sidebar';
+import Header from '../../../components/header';
 const Materials = () => {
   return (
     <div className="page d-flex">
       <SideBar />
+      <div className="content w-full">
+        <Header />
+      </div>
     </div>
   );
 }
