f8324f8fed670f0b6d4fcef0691b4166da1012c2
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-14 20:34:21 +0300

    Add the expanded card

--- frontend/src/pages/class/components/UnitCard.jsx
+++ frontend/src/pages/class/components/UnitCard.jsx
@@ -1,21 +1,37 @@
-import { faArrowDown, faBook } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowDown,
+  faArrowUp,
+  faBook,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useState } from "react";
 
 const UnitCard = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
   return (
-    <div className="unit-card">
-      <div className="unit-icon">
-        <FontAwesomeIcon icon={faBook} />
+    <div className={`unit-card ${isExpanded ? "expanded" : ""}`}>
+      <div className="unit-header" onClick={toggleExpand}>
+        <div className="unit-icon">
+          <FontAwesomeIcon icon={faBook} />
+        </div>
+        <div className="unit-content">
+          <p>ali</p>
+        </div>
+        <div className="unit-actions">
+          <button className="expand-button">
+            {isExpanded ? (
+              <FontAwesomeIcon icon={faArrowUp} />
+            ) : (
+              <FontAwesomeIcon icon={faArrowDown} />
+            )}
+          </button>
+        </div>
       </div>
-      <div className="unit-content">
-        <p>ali</p>
-      </div>
-      <div className="unit-actions">
-        <button className="expand-button">
-          <FontAwesomeIcon icon={faArrowDown} />
-        </button>
-      </div>
+      {isExpanded && <div className="unit-body">ali</div>}
     </div>
   );
 };
