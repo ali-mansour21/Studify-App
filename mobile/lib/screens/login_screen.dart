4f97ed643c1e6aa1382a84602ef11330e4bc35ab
Ali Mansour <alialhadimansour21@gmail.com>
2024-05-19 16:13:29 +0300

    Fix the login text fields

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -19,6 +19,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false;
+
   void _login() async {
     final email = _emailController.text;
     final password = _passwordController.text;
@@ -62,81 +63,91 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return AuthLayout(
-        imagePath: 'assets/Studify-logo.png',
-        title: "Welcome Back",
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Stack(
-            children: <Widget>[
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  CustomTextFormField(
+      imagePath: 'assets/Studify-logo.png',
+      title: "Welcome Back",
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 70,
+                  child: CustomTextFormField(
                     controller: _emailController,
                     labelText: "Email",
                     iconData: Icons.email,
                     keyboardType: TextInputType.emailAddress,
                   ),
-                  CustomTextFormField(
+                ),
+                const SizedBox(height: 15),
+                SizedBox(
+                  height: 70,
+                  child: CustomTextFormField(
                     controller: _passwordController,
                     labelText: "Password",
                     iconData: Icons.lock,
                     obscureText: true,
                   ),
-                  const SizedBox(height: 20),
-                  MainButton(
-                      buttonColor: const Color(0xFF3786A8),
-                      buttonText: "Login",
-                      onPressed: _login),
-                  const SizedBox(height: 20),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/register');
-                    },
-                    child: const Text(
-                      'New user? Sign Up',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ],
-              ),
-              if (_isLoading)
-                Positioned(
-                  top: -10,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    height: MediaQuery.of(context).size.height * 0.2,
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.8),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 1,
-                          blurRadius: 10,
-                          offset: const Offset(0, 5),
-                        ),
-                      ],
-                    ),
-                    child: const Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        CircularProgressIndicator(
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Color(0xFF3786A8)),
-                        ),
-                        SizedBox(height: 20),
-                      ],
-                    ),
+                ),
+                const SizedBox(height: 20),
+                MainButton(
+                  buttonColor: const Color(0xFF3786A8),
+                  buttonText: "Login",
+                  onPressed: _login,
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  child: const Text(
+                    'New user? Sign Up',
+                    style: TextStyle(color: Colors.black),
                   ),
                 ),
-            ],
-          ),
-        ));
+              ],
+            ),
+            if (_isLoading)
+              Positioned(
+                top: -10,
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 10,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: const Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(Color(0xFF3786A8)),
+                      ),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
--- mobile/lib/widgets/customtextformfield.dart
+++ mobile/lib/widgets/customtextformfield.dart
@@ -6,40 +6,40 @@ class CustomTextFormField extends StatelessWidget {
   final TextInputType keyboardType;
   final bool obscureText;
   final TextEditingController? controller;
-  const CustomTextFormField(
-      {super.key,
-      required this.labelText,
-      required this.iconData,
-      this.keyboardType = TextInputType.text,
-      this.obscureText = false,
-      this.controller});
+
+  const CustomTextFormField({
+    super.key,
+    required this.labelText,
+    required this.iconData,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    this.controller,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
-      child: SizedBox(
-        width: 283,
-        height: 55,
-        child: TextFormField(
-          controller: controller,
-          decoration: InputDecoration(
-            labelText: labelText,
-            labelStyle: const TextStyle(color: Colors.black),
-            border: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: Colors.black45, width: 2.0),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            prefixIcon: Icon(
-              iconData,
-              color: const Color(0x000000).withOpacity(0.45),
-            ),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(color: Colors.black),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
           ),
-          keyboardType: keyboardType,
-          obscureText: obscureText,
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.black45, width: 2.0),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          prefixIcon: Icon(
+            iconData,
+            color: const Color(0x000000).withOpacity(0.45),
+          ),
         ),
+        keyboardType: keyboardType,
+        obscureText: obscureText,
+        maxLines: 1,
       ),
     );
   }
